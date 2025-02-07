86072d9106532d34673476dc1cde10134a29c1b8
Sk-ILLish-GIT <sksahilparvez2000@gmail.com>
2025-02-08 01:54:31 +0530

    first-commit

---
+++ src/main/java/com/web/chat/Controller/UserController.java
@@ -0,0 +1,26 @@
+package com.web.chat.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.chat.Service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public String user(){
+        return userService.getUser();
+    }
+
+}
---
+++ src/main/java/com/web/chat/Service/UserService.java
@@ -0,0 +1,11 @@
+package com.web.chat.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public static String getUser() {
+       return "ME";
+    }
+}
