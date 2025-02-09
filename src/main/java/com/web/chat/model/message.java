2fb68b3def6a85bb66c4cc10e0404a180d5c21cc
AdithiKashain <94888785+AdithiKashain@users.noreply.github.com>
2025-02-09 17:31:45 +0530

    Create message.java

---
+++ src/main/java/com/web/chat/model/message.java
@@ -0,0 +1,33 @@
+package com.web.chat.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long message_id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
